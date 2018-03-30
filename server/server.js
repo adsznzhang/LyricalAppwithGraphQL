e17f5b46d40715d7a0624d5272ce71f75beb3afb
adsznzhang <adszn@hotmail.com>
2018-03-30 21:50:07 +1200

    Initial Commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://adsznzhang:Zhangzhen12@ds213239.mlab.com:13239/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
