a9bf821bcc47722dbc03ece1da03e37aa088f4ba
adsznzhang <adszn@hotmail.com>
2018-04-15 23:24:35 +1200

    Add input

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -2,11 +2,28 @@ import React, { Component} from 'react';
 
 
 class LyricCreate extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      content: ''
+    }
+  }
+
+  onSubmit(event){
+    //默认的提交会把html提交到后端，所以要阻止这个事件
+    event.preventDefault();
+    
+  }
+
   render(){
     return (
-      <form >
+      <form onSubmit={this.onSubmit.bind(this)}>
         <label >Add a Lyric</label>
-        <input />
+        <input
+          value={this.state.content}
+          onChange={event => this.setState({content: event.target.value})}
+        />
       </form>
     );
   }
