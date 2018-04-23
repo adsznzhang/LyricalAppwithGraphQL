bac8d150123109ebaae52aa3983538d047ad3013
adsznzhang <adszn@hotmail.com>
2018-04-23 20:04:58 +1200

    Fix likes bug

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -7,8 +7,19 @@ import gql from 'graphql-tag';
 class LyricList extends Component {
 
 
-  onLike(id){
-    this.props.mutate({variables:{id:id}});
+  onLike(id, likes){
+    this.props.mutate({
+      variables:{id:id},
+      //加快组件预渲染
+      optimisticResponse:{
+        __typename:'Mutation',
+        likeLyric:{
+          id: id,
+          __typename: 'LyricType',
+          likes:likes + 1
+        }
+      }
+    });
   }
 
 
@@ -23,7 +34,7 @@ class LyricList extends Component {
         <li key={id} className="collection-item">
           {content}
           <div className="vote-box">
-            <i className="material-icons" onClick={() => this.onLike(id)}>thumb_up</i>
+            <i className="material-icons" onClick={() => this.onLike(id,likes)}>thumb_up</i>
             {likes}
           </div>
         </li>
